83d612f2ced9356fca0891b2335ffda806bfb90d
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-01-12 23:34:26 +0500

    Jira request + bot message

--- Quest_giver.py
+++ Quest_giver.py
@@ -1,5 +1,29 @@
 import discord
 import os
+from jira import JIRA
+import json
+
+#Authorization
+with open('authorization.json') as f:
+    templates = json.load(f)
+
+print(templates)
+
+print('1')
+
+print('3')
+#Подключение к нашей Jira
+jira_host='https://quest-giver.atlassian.net'
+auth_jira = JIRA(jira_host, basic_auth=(username, api_token))
+print('4')
+print('Введите JQL запрос для выбора задач')
+jql_string='project = QG order by created DESC'
+jira_list = auth_jira.search_issues(jql_string)
+print('Jira first task:')
+print(jira_list[0])
+print('Jira list:')
+print(jira_list)
+
 
 client = discord.Client()
 
