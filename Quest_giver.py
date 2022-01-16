8558de6cfec553364cb5ff62a8ff4b7f1f123221
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-01-17 01:33:20 +0500

    everything without messages

--- Quest_giver.py
+++ Quest_giver.py
@@ -1,15 +1,25 @@
+from calendar import different_locale
+from email import message
 import discord
 import os
 from jira import JIRA
 import json
 from os import getenv
 from dotenv import load_dotenv
+import psycopg2
 
+
+
+#PGSQL environment
+uri = getenv('URI')
+#Connected to DB
+conn = psycopg2.connect(uri, sslmode='require')
+cur = conn.cursor()
 #Authorization
 with open('authorization.json') as f:
     dict_authorization = json.loads(f.read())
 
-#Heroku authorization
+#Heroku authorization environment
 jira_token = getenv('API_TOKEN_JIRA')
 jira_username = getenv('USERNAME_JIRA')
 discord_token = getenv('DISCORD_TOKEN')
@@ -20,14 +30,16 @@ auth_jira = JIRA(jira_host, basic_auth=(jira_username, jira_token))
 jql_string ='project = QG order by created DESC'
 jira_list_raw = auth_jira.search_issues(jql_string)
 
-#Создание словаря
-jira_list = {}
+#Create issues list from Jira
+# jira_list = []
+# for issue in jira_list_raw:
+#     list_issue = [str(issue), issue.fields.status.name, issue.fields.summary]
+#     jira_list.append(list_issue)
+
+dict_jira = {}
 for issue in jira_list_raw:
-    dict_issue = {}
-    #dict_issue['key'] = issue
-    dict_issue['status'] = issue.fields.status.name
-    dict_issue['summary'] = issue.fields.summary
-    jira_list[str(issue)] = dict_issue
+    dict_jira[str(issue)] = [issue.fields.status.name, issue.fields.summary]
+print(dict_jira)
 
 #Подключение к Discord
 client = discord.Client()
@@ -35,36 +47,70 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+
+#Write DB
+def write_db():
+    cur.execute ("""
+    INSERT INTO public.jira_tasks (key, status, summary) VALUES
+    ('QG-4', 'Working', 'Работа пример');
+    """)
+    conn.commit()
+
+#Read DB
+def read_db():
+    cur.execute ("""
+    SELECT key, status, summary
+	FROM public.jira_tasks;;
+    """)
+    list_result = cur.fetchall()
+    dict_db = {}
+    for issue in list_result:
+        dict_db[issue[0]] = [issue[1], issue[2]]
+    return dict_db
+dict_db_save = read_db()
+print(read_db())
+
+#Difference keys. Find new tasks.
+def diff_keys():
+    keys_db = list(read_db().keys())
+    keys_jira = list(dict_jira.keys())
+    difference = [x for x in keys_db + keys_jira if x not in keys_db]
+    return difference
+print(diff_keys())
+
+#Changed status.
+def change_status():
+    announce_dict = {}
+    for key in dict_jira.keys():
+        if key in dict_db_save and dict_jira[key][0] != dict_db_save[key][0]:
+           announce_dict[key] = dict_jira[key][0]
+    return announce_dict
+
+
+
+print(change_status())
+  
+#print(change_status())
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-#Message
+    #Message
     if message.content.startswith('hello'):
         await message.channel.send('Hello, world!')
-    if message.content.startswith('jira'):
-        await message.channel.send(jira_list)
-
-#Write_DB
-def write_db():
-    with open('database.json', 'w', encoding='utf8') as f:
-        json.dump(jira_list, f, ensure_ascii=False)
-
-#Read_DB
-def read_db():
-    with open('database.json', 'r', encoding='utf8') as f:
-        return json.loads(f.read())
-#print(read_db())
-
-#Compare. Find new tasks
-differece = set(jira_list.keys()) - set(read_db().keys())
-print(differece)
 
 
 
-#Запуск бота
-client.run(discord_token)
+
+
+
+
+
+#Launch bot
+#client.run(discord_token)
 
 
 
